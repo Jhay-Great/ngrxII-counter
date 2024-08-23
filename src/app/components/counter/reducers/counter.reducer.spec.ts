66908f8e4a623a4fb518e5a0abe85f42ac271601
Jhay-Great <alemonai.jenef@gmail.com>
2024-08-23 08:01:47 +0000

    updates test functions

--- src/app/components/counter/reducers/counter.reducer.spec.ts
+++ src/app/components/counter/reducers/counter.reducer.spec.ts
@@ -1,40 +1,46 @@
 
-import { counterReducer } from "./counter.reducer";
+import { _counterReducer } from "./counter.reducer";
 import { increment, decrement, reset } from "../actions/counter.action";
 
 describe('Counter Reducer', () => {
 
     it('should increment the state by 1', () => {
-        let state = 0
+        let state = 0;
+        let payload = {
+            interval: 1,
+        }
 
-        state = counterReducer(state, increment);
+        state = _counterReducer(state, increment(payload));
         expect(state).toBe(1);
 
-        state = counterReducer(state, increment);
+        state = _counterReducer(state, increment(payload));
         expect(state).toBe(2);
 
-        state = counterReducer(state, increment);
+        state = _counterReducer(state, increment(payload));
         expect(state).toBe(3);
         
     });
     it('should decrease the state by 1', () => {
-        let state = 5
+        let state = 5;
+        let payload = {
+            interval: 1,
+        }
 
-        state = counterReducer(state, decrement);
+        state = _counterReducer(state, decrement(payload));
         expect(state).toBe(4);
 
-        state = counterReducer(state, decrement);
+        state = _counterReducer(state, decrement(payload));
         expect(state).toBe(3);
 
-        state = counterReducer(state, decrement);
+        state = _counterReducer(state, decrement(payload));
         expect(state).toBe(2);
         
     });
     it('should reset the state to 0', () => {
 
-        expect(counterReducer(0, reset)).toBe(0);
+        expect(_counterReducer(0, reset)).toBe(0);
 
-        expect(counterReducer(3, reset)).toBe(0);
+        expect(_counterReducer(3, reset)).toBe(0);
 
     });
 
